6b7c8b605066630a7843daaac20bcb6b0edafb2c
kaotu <kao-tu@hotmail.co.th>
2020-07-05 21:56:03 +0700

    Add config and setting of WSL 🌚

---
+++ ubuntu-WSL/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install git-core zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install powerline fonts-powerline -y
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sudo apt-get install python3-pip -y
